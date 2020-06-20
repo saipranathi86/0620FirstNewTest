654c290026bd7a2c49164d7b05fdd04ff49f312e
Sai Pranathi <Sai Pranathi@SaiPranathi>
2020-06-20 21:02:42 +0530

    Test Script Modify 1

--- Project20/src/FirstClassGitone.java
+++ Project20/src/FirstClassGitone.java
@@ -4,10 +4,7 @@ public class FirstClassGitone {
 	public static void main(String[] args) {
 		System.out.println("Test one");
 		
-		System.out.println("Confusion");
 		
-		System.out.println("Hi");
-
 	}
 
 }
